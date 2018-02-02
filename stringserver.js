91afe9650e187f952c557695d40008baaedcf4a4
Felice Gardner <felice.gardner@gmail.com>
2018-02-01 21:15:42 -0500

    Modified stringserver message

--- stringserver.js
+++ stringserver.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.write("Hello World");
+    res.write("Hello Larry and TA Team,
+               This is the first assignment in cscie31.
+               Looking forward to the class!");
     res.end();
 });
 server.listen(8080);
