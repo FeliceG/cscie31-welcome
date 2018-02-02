86e7952b8975b28a88b3daf6fc7cd6a1e136ed1e
Felice Gardner <felice.gardner@gmail.com>
2018-02-01 22:15:08 -0500

    Modified stringerserver again.

--- stringserver.js
+++ stringserver.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.write("Hello Larry and TA Team,
-               This is the first assignment in cscie31.
-               Looking forward to the class!");
+    res.write('Hello Larry and TA Team,');
+    res.write('This is the first assignment in cscie31.');
+    res.write('Looking forward to the class!');
     res.end();
 });
 server.listen(8080);
