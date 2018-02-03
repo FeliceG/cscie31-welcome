efd46d21ec69617f214fc261600f607ffe0b1131
Felice Gardner <felice.gardner@gmail.com>
2018-02-03 09:49:12 -0500

    Modified stringserver again to clean.

--- stringserver.js
+++ stringserver.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.writeHead(200);
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<strong>Hello Larry and TA Team,</strong><br><br>');
     res.write('<i>This is the first assignment in cscie31.</i><br><br>');
     res.write('<i>Looking forward to the class!</i><br><br>');
